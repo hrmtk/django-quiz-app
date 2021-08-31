b84ef42f9e85cea57df29934ffac21825f4baeea
hrmtk <morgenzonne@zoho.com>
2021-08-31 20:44:22 +0900

    Add timer

--- quiz/static/quiz/question.js
+++ quiz/static/quiz/question.js
@@ -1,13 +1,14 @@
-const url = window.location.href
-const quizBox = document.getElementById("quiz-box")
-const alertBox = document.getElementById('alert-box')
+// Get data
+const url = window.location.href;
+const quizBox = document.getElementById("quiz-box");
+const alertBox = document.getElementById('alert-box');
 
 $.ajax({
 	type: 'GET',
 	url: `${url}/detail`,
 	success: function(res) {
-		const data = res.data
-		const questions = res.questions
+		const data = res.data;
+		const questions = res.questions;
 		questions.forEach(el => {
 			Object.keys(el).forEach(function(key) {
 
@@ -30,25 +31,28 @@ $.ajax({
 				`
 			})
 		})
+		timer(10);
 	},
 	error: function(error) {
-		console.log('error', error)
+		console.log('error', error);
 	}
 })
 
-const quizForm = document.getElementById('quiz-form')
-const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+// Send answer
+const quizForm = document.getElementById('quiz-form');
+const csrf = document.getElementsByName('csrfmiddlewaretoken');
 
 const sendAnswer = () => {
-	const elements = [...document.getElementsByClassName('answer')]
-	const answered = {}
-	answered['csrfmiddlewaretoken'] = csrf[0].value
+	const elements = [...document.getElementsByClassName('answer')];
+	const answered = {};
+	answered['csrfmiddlewaretoken'] = csrf[0].value;
 	elements.forEach(el => {
 		if (el.checked) {
-			answered[el.name] = el.value
+			answered[el.name] = el.value;
 		} else {
 			if (!answered[el.name]) {
-				answered[el.name] = null
+				answered[el.name] = null;
 			}
 		}
 	})
@@ -58,8 +62,8 @@ const sendAnswer = () => {
 		url: `${url}/save`,
 		data: answered,
 		success: function(res) {
-			results = res.results
-			score = res.score
+			results = res.results;
+			score = res.score;
 
 			results.forEach(result => {
 				for (const [question, message] of Object.entries(result)) {
@@ -67,7 +71,7 @@ const sendAnswer = () => {
 					ansChecked.innerText = message;
 				}
 			})
-			handleAlerts('success', 'success')
+			handleAlerts('success', 'success');
 		},
 		error: function(error) {
 			console.log(error);
@@ -76,6 +80,41 @@ const sendAnswer = () => {
 }
 
 quizForm.addEventListener('submit', e=> {
-	e.preventDefault()
-	sendAnswer()
+	e.preventDefault();
+	sendAnswer();
 })
+
+
+// Countdown timer
+let countdown;
+const timerDisplay = document.querySelector('.time-left');
+
+function timer(seconds) {
+	clearInterval(countdown);
+	let timePassed = 0;
+	seconds++;
+
+	countdown = setInterval(() => {
+		timePassed += 1;
+		let timeLeft = seconds - timePassed;
+		if (timeLeft === 0) {
+			timerDisplay.innerHTML = '00:00';
+			clearInterval(countdown);
+			alert('Time over');
+			sendAnswer();
+		}
+		timerDisplay.innerHTML = formatTime(timeLeft);
+	}, 1000);
+}
+
+function formatTime(seconds) {
+	let minutes = Math.floor(seconds / 60);
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+	let secs = seconds % 60;
+	if (secs < 10) {
+		secs = `0${secs}`;
+	}
+	return `${minutes}:${secs}`;
+}
